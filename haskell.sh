0fe163f5fd59a6fe4cba52bb257cc6b49cf06bb7
Andrew Cowie <andrew@operationaldynamics.com>
2017-11-02 13:40:02 +1100

    Add usage example to haskell wrapper

--- haskell.sh
+++ haskell.sh
@@ -1,40 +1,69 @@
 #!/bin/bash
 
-set -x
-
 MODE="$1"
 shift
 
-stack build --only-dependencies
-
 case "$MODE" in
 "build")
-	exec ghcid \
+	set -x
+	stack build --only-dependencies
+	ghcid \
 		--no-title \
 		--command="stack exec -- ghci -ilib:src:tests -Wall ${1}" \
 ;;
 
 "run")
-	exec ghcid \
+	set -x
+	stack build --only-dependencies
+	ghcid \
 		--no-title \
 		--command="stack exec -- ghci -ilib:src:tests ${1}" \
 		--test=":main ${*:2}"
 ;;
 
 "test")
-	exec ghcid \
+	set -x
+	stack build --only-dependencies
+	ghcid \
 		--no-title \
 		--command="stack exec -- ghci -ilib:src:tests ${1}" \
 		--test=":main --color ${*:2}"
 ;;
 
 "explore")
-	exec stack exec -- ghci -ilib:src:tests "${1}"
+	set -x
+	stack build --only-dependencies
+	stack exec -- ghci -ilib:src:tests "${1}"
 ;;
 
 *)
-	echo "Need a mode; one of {build, run, explore}"
-	exit 1
+	cat <<HERE
+Usage: `basename $0` <mode> <filename>
 
+Where mode is one of:
+  build		Continuously build the given Haskell file.
+
+  run		Build the given source file, then on success,
+		run the \`main\` function. When the sources
+		change the process will be terminated and the
+		code rebuilt.
+
+  test		Same, but appends \`--color\` to arguments so
+		hspec's output is colourized.
+
+  explore	Build the given source file and its dependencies,
+		then on success, fire up GHCi with the given
+		module loaded.
+
+Examples:
+	$ haskell build lib/Data/Time/Gregorian.hs
+	$ haskell run src/AlarmClock.hs
+	$ haskell explore tests/Experiment.hs
+	$ haskell test tests/TestSuite.hs
+
+HERE
+	exit 1
+	;;
 esac
 
+exit 0
